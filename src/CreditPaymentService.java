b1efd1913cfda8c435b4227281be4f0d987625f6
Nezumi-sama <kryska007@yandex.ru>
2022-05-14 19:43:10 +0300

    Modify CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,14 +1,15 @@
 public class CreditPaymentService {
     public int calculate(int year, int amount) {
-        int credit;
-        if (year == 1) {
-            credit = (int) (amount + amount * 0.054932) / 12;
-        } else if (year == 2) {
-            credit = (int) (amount + amount * 0.05368 * 2) / 24;
-        } else {
-            credit = (int) (amount + amount * 0.0538106666666667 * 3) / 36;
-        }
+        int monthlyPayment;
+        double monthPercent, k;
 
-        return credit;
+//расчет % в месяц в долях
+        monthPercent = 9.99 / (100 * 12);
+//расчет коэффициента
+        k = monthPercent + monthPercent / (Math.pow(1 + monthPercent, year * 12) - 1);
+//расчет ежемесячного платежа
+        monthlyPayment = (int) (k * amount);
+
+        return monthlyPayment;
     }
 }
